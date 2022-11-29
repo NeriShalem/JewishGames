5850d94c0453835ba20917d52d47a0100bf2b0ac
Eliyahu-Kriel <eliyahu.k@phytech.com>
2022-11-30 00:12:26 +0200

    fix some styles in games page & crate games constant outside the game page

--- src/Pages/GamesPage.jsx
+++ src/Pages/GamesPage.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import "./GamesPage.css";
-import memoryLogoGame from "../assets/img/logo/memoryLogoGame.svg";
-import TetrisLogoGame from "../assets/img/logo/TetrisLogoGame.svg";
-import TicTacToeLogoGame from "../assets/img/logo/TicTacToeLogoGame.svg";
-import SoonLogoGame from "../assets/img/logo/SoonLogoGame.svg";
-
 import { Link } from "react-router-dom";
-import TitleTextPage from "./TitleTextPage";
 
-function GamesPage(props) {
+import TitleTextPage from "./TitleTextPage";
+import { GAMES_LIST } from "../constants/games";
+import "./GamesPage.css";
+
+const GamesPage = () => {
+   const renderGame = ({ url, name, imgUrl}, index) => (
+       <div className="squareGameLink" key={index}>
+           <Link to={url}>
+               <img className="GameImgLink" src={imgUrl} alt="TicTacToeLogoGame" />
+           </Link>
+           <Link className="LinkGameLink" to={url}>
+               {name}
+           </Link>
+       </div>
+   )
+
   return (
     <>
       <div className="gamePage">
@@ -16,79 +24,7 @@ function GamesPage(props) {
           <TitleTextPage title={"Games"} />
         </div>
         <div className="gameListBoard">
-          <div className="gameList">
-            <div className="squareGameLink">
-              <Link to="/TicTacToe">
-                <img className="GameImgLink" src={TicTacToeLogoGame} alt="TicTacToeLogoGame" />
-              </Link>
-              <Link className="LinkGameLink" to="/TicTacToe">
-                TicTacToe
-              </Link>
-            </div>
-
-            <div className="squareGameLink">
-              <Link to="/Tetris">
-                <img className="GameImgLink" src={TetrisLogoGame} alt="TetrisLogoGame" />
-              </Link>
-              <Link className="LinkGameLink" to="/Tetris">
-                Tetris
-              </Link>
-            </div>
-
-            <div className="squareGameLink">
-              <Link to="/MemoryGame">
-                <img className="GameImgLink" src={memoryLogoGame} alt="memoryLogoGame" />
-              </Link>
-              <Link className="LinkGameLink" to="/MemoryGame">
-                Memory Game
-              </Link>
-            </div>
-
-
-            <div className="squareGameLink">
-              <Link to="/Soon">
-                <img className="GameImgLink" src={SoonLogoGame} alt="SoonLogoGame" />
-              </Link>
-              <Link className="LinkGameLink" to="/Soon">
-                Soon
-              </Link>
-            </div>
-            <div className="squareGameLink">
-              <Link to="/Soon">
-                <img className="GameImgLink" src={SoonLogoGame} alt="SoonLogoGame" />
-              </Link>
-              <Link className="LinkGameLink" to="/Soon">
-                Soon
-              </Link>
-            </div>
-            <div className="squareGameLink">
-              <Link to="/Soon">
-                <img className="GameImgLink" src={SoonLogoGame} alt="SoonLogoGame" />
-              </Link>
-              <Link className="LinkGameLink" to="/Soon">
-                Soon
-              </Link>
-            </div>
-            <div className="squareGameLink">
-              <Link to="/Soon">
-                <img className="GameImgLink" src={SoonLogoGame} alt="SoonLogoGame" />
-              </Link>
-              <Link className="LinkGameLink" to="/Soon">
-                Soon
-              </Link>
-            </div>
-            <div className="squareGameLink">
-              <Link to="/Soon">
-                <img className="GameImgLink" src={SoonLogoGame} alt="SoonLogoGame" />
-              </Link>
-              <Link className="LinkGameLink" to="/Soon">
-                Soon
-              </Link>
-            </div>
-
-
-
-          </div>
+          {GAMES_LIST.map(renderGame)}
         </div>
       </div >
     </>
---
+++ src/constants/games.js
@@ -0,0 +1,47 @@
+import ticTacToeLogoGame from "../assets/img/logo/TicTacToeLogoGame.svg";
+import tetrisLogoGame from "../assets/img/logo/TetrisLogoGame.svg";
+import memoryLogoGame from "../assets/img/logo/memoryLogoGame.svg";
+import soonLogoGame from "../assets/img/logo/SoonLogoGame.svg";
+
+export const GAMES_LIST = [
+    {
+        name: 'TicTacToe',
+        url: '/TicTacToe',
+        imgUrl: ticTacToeLogoGame
+    },
+    {
+        name: 'Tetris',
+        url: '/Tetris',
+        imgUrl: tetrisLogoGame
+    } ,
+    {
+        name: 'Memory Game',
+        url: '/MemoryGame',
+        imgUrl: memoryLogoGame
+    },
+    {
+        name: 'Soon',
+        url: '/Soon',
+        imgUrl: soonLogoGame
+    },
+    {
+        name: 'Soon',
+        url: '/Soon',
+        imgUrl: soonLogoGame
+    },
+    {
+        name: 'Soon',
+        url: '/Soon',
+        imgUrl: soonLogoGame
+    },
+    {
+        name: 'Soon',
+        url: '/Soon',
+        imgUrl: soonLogoGame
+    },
+    {
+        name: 'Soon',
+        url: '/Soon',
+        imgUrl: soonLogoGame
+    }
+]
